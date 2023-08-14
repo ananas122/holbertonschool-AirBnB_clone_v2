c82ce19961ff04c15eef0662d9433c108e0780f4
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-14 11:24:52 +0200

    Replace underscores (_) with spaces in the text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text():
     "Replace underscores (_) with spaces in the text "
-    return f'C {text.replace('_', ' ')}'
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
